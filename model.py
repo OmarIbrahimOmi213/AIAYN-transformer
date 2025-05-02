4e0419d6c19c8ce370d52de5dc9b3f58216a0342
Maged Serag <bayoummf@kaust.edu.sa>
2025-05-02 15:33:08 +0300

    Initial commit with code and README

---
+++ model.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+import math
+import numpy as np
+
+class WordEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        pe = torch.zeros(seq_len, d_model)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        denom = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float) * -math.log(10000.0) / d_model)
+        pe[:, 0::2] = torch.sin(position * denom)
+        pe[:, 1::2] = torch.cos(postion * denom)
+
+        pe = pe.unequeeze(0)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x += self.pe[:, :x.shape[1], :].requires_grad(False)
+        return self.dropout(x)
+
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 1e-6) -> None:
+        super().__init__():
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim=True)
+        std = x.std(dim = -1, keepdim=True)
+        return self.alpha * (x - mean)/(self.std + eps) + self.bias
